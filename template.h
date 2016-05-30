4f66b1405deaf914b9b4b02a9e3fcffc39040d46
xuxj1005 <xuxj1005@126.com>
2016-05-30 21:16:47 +0800

    several compiling bugs fixed.

--- template.h
+++ template.h
@@ -4,12 +4,18 @@
 #include <functional>
 #include <cstddef>
 #include <cassert>
+#include <vector>
 namespace DataStructure
 {
 	template<class T>
 	class Link_cut_cactus
 	{
 	public:
+		class Node;
+		class Edge;
+		class Circle;
+		class Lcc_message;
+
 		class Node
 		{
 		public:
@@ -23,9 +29,10 @@ namespace DataStructure
 		
 			int delta;
 		
-			bool is_root(Node *x)
+			bool is_root()
 			{
-				return !(x -> par && (x -> par -> ls == x || x -> par -> rs == x));
+				return !(par && (par->ls == this || par->rs == this));
+				//return !(x -> par && (x -> par -> ls == x || x -> par -> rs == x));
 			}
 			void make_rev()
 			{
@@ -53,16 +60,28 @@ namespace DataStructure
 			}
 			void all_pushdown()
 			{
-				vector<Node*> tmp;
-				for (Node *i = this; !i -> is_root(); i = i -> fa)
+				std::vector<Node*> tmp;
+				for (Node *i = this; !i -> is_root(); i = i -> par)
 					tmp.push_back(i);
 				for (int i = tmp.size() - 1; i >= 0; i--)
 					tmp[i] -> pushdown();
 				tmp.clear();
 			}
-			void zig(Node *x)
+			void zig(/*Node *x*/)
 			{
-				Node *par = x -> par;
+				if (par->par){
+					if (par->par->ls == par) par->par->ls = this;
+					else if (par->par->rs == par) par->par->rs = this;
+				}
+				par = par->par;
+				par->par = this;
+				par->ls = rs;
+				if (rs) rs->par = par;
+				rs = par;
+
+				par->maintain();//
+				maintain();//
+				/*Node *par = x -> par;
 				if(par -> par)
 				{
 					if(par -> par -> ls == par) par -> par -> ls = x;
@@ -75,13 +94,26 @@ namespace DataStructure
 				if(x -> rs) x -> rs -> par = par;
 				x -> rs = par;
 			
-				maintain(par);//
-				maintain(x);//
+				par->maintain();//
+				x->maintain();//*/
 			}
 		
-			void zag(Node *x)
+			void zag(/*Node *x*/)
 			{
-				Node *par = x -> par;
+				if (par->par){
+					if (par->par->ls == par) par->par->ls = this;
+					else if (par->par->rs == par) par->par->rs = this;
+				}
+
+				par = par->par;
+				par->par = this;
+				par->rs = ls;
+				if (ls) ls->par = par;
+				ls = par;
+
+				par->maintain();//
+				maintain();//
+				/*Node *par = x -> par;
 				if(par -> par)
 				{
 					if(par -> par -> ls == par) par -> par -> ls = x;
@@ -94,8 +126,8 @@ namespace DataStructure
 				if(x -> ls) x -> ls -> par = par;
 				x -> ls = par;
 			
-				maintain(par);//
-				maintain(x);//
+				par->maintain();//
+				x->maintain();//*/
 			}
 		
 			void rotate()
@@ -109,7 +141,7 @@ namespace DataStructure
 				all_pushdown();
 				while(!is_root())
 				{
-					if(par -> is_root()) rotate(x);
+					if(par -> is_root()) rotate();
 					else
 					{
 						bool t1 = (par -> ls == this), t2 = (par -> par -> ls == par);
@@ -131,7 +163,7 @@ namespace DataStructure
 				Node *x = this;
 				while(x -> ls)
 				{
-					pushdown(x), x = x -> ls;
+					x->pushdown(), x = x -> ls;
 				}
 				x -> splay();
 				return x;
@@ -143,6 +175,7 @@ namespace DataStructure
 		};
 		class Weight
 		{
+		public:
 			int wa; T wb;
 			friend bool operator == (const Weight &u, const Weight &v)
 			{
@@ -185,9 +218,9 @@ namespace DataStructure
 			{
 				mina = a, minb = b;
 			}
-			Path_message merge(const Path_messgae &u, const Path_message &v)
+			Path_message merge(const Path_message &u, const Path_message &v)
 			{
-				return Path_message(u.minA + v.minA, min(u.minB, v.minB));
+				return Path_message(u.mina + v.mina, std::min(u.minb, v.minb));
 			}
 			Path_message better(const Path_message &u, const Path_message &v)
 			{
@@ -220,7 +253,9 @@ namespace DataStructure
 				//TODO
 			}
 		};
-		Node **node, **edge, **circle;
+		Node **node;
+		Edge **edge;
+		Circle **circle;
 		size_t node_size, edge_size, circle_size;
 		Link_cut_cactus() {} = default;
 		Link_cut_cactus(const int &n)
@@ -228,22 +263,22 @@ namespace DataStructure
 			node_size = n;
 			edge_size = 0;
 			circle_size = 0;
-			**node = new Node* [n + 10];
+			node = new Node* [n + 10];
 			for (int i = 0; i <= n; i++)
 				node[i] = new Node();
-			**edge = new Edge* [2 * n + 10];
-			**circle = new Circle* [2 * n + 10];
+			edge = new Edge* [2 * n + 10];
+			circle = new Circle* [2 * n + 10];
 		}
 		
-		~Link_cut_cactus
+		~Link_cut_cactus()
 		{
-			for (int i = 0; i <= node_sz; i++)
+			for (int i = 0; i <= node_size; i++)
 				if(node[i]) delete(node[i]);
 			delete  node;
-			for (int i = 0; i <= edge_sz; i++)
+			for (int i = 0; i <= edge_size; i++)
 				if(edge[i]) delete(edge[i]);
 			delete  edge;
-			for (int i = 0; i <= circle_sz; i++)
+			for (int i = 0; i <= circle_size; i++)
 				if(circle[i]) delete(circle[i]);
 			delete  circle;
 		}
@@ -259,7 +294,7 @@ namespace DataStructure
 		{
 			//TODO
 		}
-		pair<int, T> query(int u, int v)
+		std::pair<int, T > query(int u, int v)
 		{
 			//TODO
 		}
